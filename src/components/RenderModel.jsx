dff9217f102b0bc8bd6503dd744b843db98512e1
kundra-raghav <kundra.raghav@gmail.com>
2024-05-06 16:43:50 +0530

    Initial commit: Adding Next.js project

---
+++ src/components/RenderModel.jsx
@@ -0,0 +1,20 @@
+"use client"
+import { Canvas } from '@react-three/fiber';
+import React, {Suspense} from 'react';
+import clsx from 'clsx';
+import { Environment } from '@react-three/drei';
+
+const RenderModel = ({ children, className }) => {
+  return (
+    <Canvas
+      className={clsx("w-screen h-screen -z-10 relative", className)}
+    >
+      <Suspense fallback={null}>
+        {children}
+      </Suspense>
+      <Environment preset='dawn'/>
+    </Canvas>
+  );
+};
+
+export default RenderModel;
